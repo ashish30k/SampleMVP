17d1b858ad64d8fbec97e9d2238d8162683f0c3d
ashishkumar <kumarashish.knit@gmail.com>
2017-08-28 00:49:42 -0400

    using Asynctask.

--- app/src/main/java/com/example/ashishkumar/samplemvp/interactors/LoginInteractorImpl.java
+++ app/src/main/java/com/example/ashishkumar/samplemvp/interactors/LoginInteractorImpl.java
@@ -1,5 +1,6 @@
 package com.example.ashishkumar.samplemvp.interactors;
 
+import android.os.AsyncTask;
 import android.text.TextUtils;
 
 /**
@@ -7,21 +8,39 @@ import android.text.TextUtils;
  */
 
 public class LoginInteractorImpl implements LoginInteractor {
+
+    private LoginListener loginListener;
+
     @Override
     public void login(String userName, String password, LoginListener loginListener) {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        this.loginListener = loginListener;
 
         if (loginListener != null) {
-            if (!TextUtils.isEmpty(userName) && !TextUtils.isEmpty(password)) {
+            new LoginAsyncTask().execute(userName, password);
+        }
+
+    }
+
+    private class LoginAsyncTask extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            String userName = strings[0];
+            String password = strings[1];
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return !TextUtils.isEmpty(userName) && !TextUtils.isEmpty(password);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if (result) {
                 loginListener.onLoginSuccess();
             } else {
                 loginListener.onLoginFailed("Login Failed");
             }
         }
-
     }
 }
